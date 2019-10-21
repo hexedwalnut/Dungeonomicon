a3018f78c55d0c95198970cf06b25924386bc8a7
tgrifka <grifka94@yahoo.com>
2019-10-20 21:53:12 -0400

    Edited Dungeonomicon.java to allow a viewing of the DiceUi

--- src/Dungeonomicon.java
+++ src/Dungeonomicon.java
@@ -1,7 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Dungeonomicon {
+public class Dungeonomicon extends Application {
 
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DiceUI diceUI = new DiceUI();
+        Scene scene = new Scene(diceUI.getDicePane());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
