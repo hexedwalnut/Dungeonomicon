50881bf3aa97e7ce75ccd9f5565b44410c5cacaa
tgrifka <grifka94@yahoo.com>
2019-10-20 20:07:27 -0400

    Worked on basic UI, Dice buttons are not fit to the right column size

--- src/DiceUI.java
+++ src/DiceUI.java
@@ -1,6 +1,5 @@
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
