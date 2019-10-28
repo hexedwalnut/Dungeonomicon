12a375182da18c5303d0d808c8c61e6c0e559239
Daniel Masker <madly123iamme@gmail.com>
2019-10-28 15:57:15 -0400

    Set up persistence files.

---
+++ src/Persistance/FileParser.java
@@ -0,0 +1,15 @@
+package Persistance;
+
+public class FileParser {
+    public FileParser(String fileLocation) {
+
+    }
+
+    public void ParseFile() {
+
+    }
+
+    public void SaveToFile() {
+
+    }
+}
---
+++ src/Persistance/SettingsStorage.java
@@ -0,0 +1,49 @@
+package Persistance;
+
+import Initiative.Combatant;
+import Initiative.NonPlayerCharacter;
+import Initiative.PlayerCharacter;
+import Initiative.StatusEffect;
+
+import java.util.ArrayList;
+
+public class SettingsStorage {
+    private static final String fileLocation = "";
+    private static ArrayList<Combatant> combatants;
+    private static ArrayList<StatusEffect> statusEffects;
+    private static FileParser parser = new FileParser(fileLocation);
+
+    public static void Load() {
+        parser.ParseFile();
+    }
+
+    public static void Save() {
+        parser.SaveToFile();
+    }
+
+    public static ArrayList<Combatant> getCombatants() {
+        ArrayList<Combatant> output = new ArrayList<>();
+        output.addAll(combatants);
+        return output;
+    }
+
+    public static ArrayList<PlayerCharacter> getPlayerCharacters() {
+        ArrayList<PlayerCharacter> output = new ArrayList<>();
+        for(Combatant i : combatants) {
+            if(i instanceof PlayerCharacter) {
+                output.add((PlayerCharacter)i);
+            }
+        }
+        return output;
+    }
+
+    public static ArrayList<NonPlayerCharacter> getNonPlayerCharacters() {
+        ArrayList<NonPlayerCharacter> output  = new ArrayList<>();
+        for(Combatant i : combatants) {
+            if(i instanceof  NonPlayerCharacter) {
+                output.add((NonPlayerCharacter)i);
+            }
+        }
+        return output;
+    }
+}
