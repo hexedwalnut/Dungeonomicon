1cfe8073bc67522c3e100b78b8afeedf250bda80
Daniel Masker <madly123iamme@gmail.com>
2019-11-08 17:54:46 -0500

    Finished off StatusEffects class

--- src/Persistance/StatusEffects.java
+++ src/Persistance/StatusEffects.java
@@ -1,29 +1,71 @@
 package Persistance;
 
 import Initiative.StatusEffect;
-import com.sun.org.apache.xpath.internal.objects.XString;
-import com.sun.org.apache.xpath.internal.objects.XStringForChars;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
 
 /**
  * StatusEffects parses all the possible status effects from xml and stores their descriptions
  *
- * Date Last Modified: 11/7/2019
- * @author DaDarkWizard
+ * Date Last Modified: 11/8/2019
+ * @author Daniel Masker
  */
 public class StatusEffects {
     private static String fileLocation = "/Dungeonomicon/StatusEffects.xml";
     private static ArrayList<StatusEffect> effects = new ArrayList<>();
+    private static boolean initialized = false;
 
-    private StatusEffects() {
-
+    public StatusEffects() {
+        if(!initialized) {
+            Initialize();
+        }
     }
 
+    /**
+     * Loads all Status Effects from XML into Memory
+     */
     public void Initialize() {
-        if(!SettingsStorage.isInitialized()) {
-            SettingsStorage.Initialize();
-        }
+
+        //Use FileParser to get XML Document
         fileLocation = System.getenv("APPDATA") + fileLocation;
+        FileParser parser = new FileParser(fileLocation);
+        Document doc = parser.ParseFile();
+
+        //Get all effects as a list, then add them to the effects array
+        NodeList xmlEffects = doc.getDocumentElement().getElementsByTagName("statusEffect");
+        for(int i = 0; i < xmlEffects.getLength(); i++) {
+            String name = xmlEffects.item(i).getAttributes().getNamedItem("name").getNodeValue();
+            String desc = xmlEffects.item(i).getTextContent();
+            StatusEffect effect = new StatusEffect(name, desc);
+            effects.add(effect);
+        }
+    }
+
+    /**
+     * Returns all status effects
+     *
+     * @return an ArrayList of all the effects
+     */
+    public ArrayList<StatusEffect> getAllEffects() {
+        return new ArrayList<>(effects);
+    }
+
+    /**
+     * Converts all status effects to a string
+     *
+     * @return all status effects as a string
+     */
+    public String toString() {
+        StringBuilder output = new StringBuilder("Status Effects:{");
+        for(int i = 0; i < effects.size(); i++) {
+            output.append("[").append(effects.get(i).toString()).append("]");
+            if(i < effects.size() - 1) {
+                output.append(",");
+            }
+        }
+        output.append("}");
+        return output.toString();
     }
 }
