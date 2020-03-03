e953bcf644975dfd6b3cf0c82e9505a15ec5c120
hexedwalnut <jteahe02@gmail.com>
2020-03-02 19:02:27 -0500

    Fleshed out the class InitiativeTracker. Added its intend functionality.

--- src/Initiative/InitiativeTracker.java
+++ src/Initiative/InitiativeTracker.java
@@ -1,21 +1,44 @@
 package Initiative;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class InitiativeTracker {
     //Variables---------------------------------------------------------------------------------------------------------
     private ArrayList<Combatant> combatants = null; //list of combatants
+    private int cursor = 0;
 
     //Methods-----------------------------------------------------------------------------------------------------------
-
     /**
      * Moves to the next turn in initiative
      */
-    public void nextTurn(){
-        //TODO
+    public Combatant nextTurn(){
+        cursor++;
+        if(cursor > combatants.size()-1){
+            cursor = 0;
+        }
+        return combatants.get(cursor);
     }
-    //Constructors------------------------------------------------------------------------------------------------------
 
+    /**
+     * This method adds a combatant to the ArrayList of "Combatant"s
+     * @param combatant the Combatant to be added
+     */
+    public void addCombatant(Combatant combatant){
+        combatants.add(combatant);
+        Arrays.sort(combatants.toArray());
+    }
+
+    /**
+     * This method removes a combatant from the ArrayList of "Combatant"s
+     * @param combatant the combatant to be removed
+     * @return If the Combatant "combatant" was removed
+     */
+    public boolean removeCombatant(Combatant combatant){
+        return combatants.remove(combatant);
+    }
+
+    //Constructors------------------------------------------------------------------------------------------------------
     /**
      * Default Constructor
      */
