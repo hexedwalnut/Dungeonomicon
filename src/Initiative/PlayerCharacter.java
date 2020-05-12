7ad5bdadbc83e0614bbd968bb2b04be673bf1364
Daniel Masker <madly123iamme@gmail.com>
2020-05-12 08:31:22 -0600

    Finished up persistence functions and cleaned up class.

--- src/Initiative/PlayerCharacter.java
+++ src/Initiative/PlayerCharacter.java
@@ -1,12 +1,12 @@
 package Initiative;
 
-import com.sun.javafx.binding.StringFormatter;
+import Persistance.StatusEffects;
 import javafx.scene.layout.Pane;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.w3c.dom.*;
 
 import java.util.ArrayList;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
 
 public class PlayerCharacter extends Combatant {
     //Variables---------------------------------------------------------------------------------------------------------
@@ -49,15 +49,51 @@ public class PlayerCharacter extends Combatant {
 
     @Override
     public Element toXMLElement(Document doc) {
-        //Todo write this
-        return null;
+
+        //Create combatant element using the document
+        Element combatant = doc.createElement("Combatant");
+        doc.getFirstChild().appendChild(combatant);
+
+        //Add attributes to the element
+        combatant.setAttribute("type", "PlayerCharacter");
+        combatant.setAttribute("initiative", Integer.toString(this.getInitiative()));
+        combatant.setAttribute("name", this.getName());
+
+        //Add status effects to the element
+        for (StatusEffect statusEffect : getStatusEffects()) {
+            //Create the status effect element
+            Element xmlStatus = doc.createElement("StatusEffect");
+            //Set the type to standard. This will be used when we allow custom status effect types
+            xmlStatus.setAttribute("type", "standard");
+            //Set the name. As long as it's standard, this is all we need.
+            xmlStatus.setAttribute("name", statusEffect.getName());
+            //Add the effect to the main element
+            combatant.appendChild(xmlStatus);
+        }
+
+        //Return the element
+        return combatant;
     }
 
     @Override
     public void generateFromXMLNode(Node node) {
-        //Todo get name
-        //Todo get Status effects
-        //Todo get initiative
-        //Todo figure out why stuff isn't done
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element eNode = (Element) node;
+            this.setInitiative(Integer.parseInt(eNode.getAttribute("initiative")));
+            this.setName(eNode.getAttribute("name"));
+
+            NodeList statusEffectNodes = eNode.getElementsByTagName("StatusEffect");
+
+            StatusEffects allStatusEffects = new StatusEffects();
+
+            for (int i = 0; i < statusEffectNodes.getLength(); i++) {
+                if (statusEffectNodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element effect = (Element) statusEffectNodes.item(i);
+                    this.addStatusEffect(allStatusEffects.getEffectByName(effect.getAttribute("name")));
+                } else {
+                    throw new RuntimeException("YEET! Wasn't an element at PC!");
+                }
+            }
+        }
     }
 }
